dc58a80ffcd8423c715e32a468770eb3d499dd75
thibaud <thibaud@172.16.113.237>
2012-11-26 19:58:12 +0100

    final

--- vues/v_validationComptable.php
+++ vues/v_validationComptable.php
@@ -1,5 +1,5 @@
 
-
+                
 <form method="POST"  action="index.php?uc=validation&action=validationComptable"> 
         <input id="ok" type="submit" value="Valider cette fiche" size="20" />  
 </form>
